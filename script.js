90b5e6623cdb2091a8612dffdc53e5de0192c14a
MuhummadShohag <muhammadshohagislam.software.e@gmail.com>
2022-09-14 22:50:07 +0300

    Done Project

--- script.js
+++ script.js
@@ -93,12 +93,12 @@ const gameOver = () => {
     // show result
     resultModal.innerHTML += `
     <h1>Finished!</h1>
-    <p>You took: <span class="bold">${timeTakenInteger}</span> seconds</p>
+    <p>You took: <span class="bold">${timeTaken}</span> seconds</p>
     <p>You made <span class="bold red">${errorCount}</span> mistakes</p>
     <button onclick="closeModal()">Close</button>
   `;
 
-    addHistory(questionText, timeTakenInteger, errorCount);
+    addHistory(questionText, timeTaken, errorCount);
 
     // restart everything
     startTime = null;
